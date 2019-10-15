8371e002dc016efcd496aed9cf244e8bf3a266b6
SCHALASANI <saicharan.chalasani@coxautoinc.com>
2019-10-15 16:56:33 -0700

    Initial commit: project structure and basic stack

---
+++ src/main/java/BasicStack.java
@@ -0,0 +1,50 @@
+public class BasicStack<X> {
+
+    private X[] data;
+    private int stackpointer;
+
+    public BasicStack() {
+        data = (X[]) new Object[1000];
+        stackpointer = 0;
+    }
+
+    public void push(X newItem){
+        data[stackpointer++] = newItem;
+    }
+
+    public X pop(){
+        if(stackpointer == 0){
+            throw new IllegalArgumentException("No more items on the stack");
+        }
+        return data[--stackpointer];
+    }
+
+    public boolean contains(X item){
+        boolean found = false;
+
+        for(int i=0; i<stackpointer; i++){
+            if(data[i].equals(item)){
+                found = true;
+                break;
+            }
+        }
+
+        return found;
+    }
+
+    public X access(X item){
+        while (stackpointer > 0){
+            X tempItem = pop();
+            if(item.equals(tempItem)){
+                return tempItem;
+            }
+        }
+
+        // If we didn't find the item in the stack throw an exception.
+        throw new IllegalArgumentException("Could not find item on the stack" + item);
+    }
+
+    public int size(){
+        return stackpointer;
+    }
+}
